4ce9df95779888148829abaa142c29fb79b69228
Rezahans <102926853+Rezahans@users.noreply.github.com>
2022-04-12 12:47:09 +0700

    Add files via upload

---
+++ ATMBank/src/atm/Atm.java
@@ -0,0 +1,17 @@
+
+package atm;
+import form.LoginFrame;
+
+public class Atm {
+
+    
+    public static void main(String[] args) {
+     
+        LoginFrame Frame= new LoginFrame();
+        Frame.setLocationRelativeTo(null);
+        Frame.setVisible(true);
+        Frame.setResizable(false);
+        
+    }
+    
+}
---
+++ ATMBank/src/core/Customer.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Customer{
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
